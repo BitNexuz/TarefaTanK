17ae99e5071eb64668c8969e9191839585b9bff2
BitNexuz <54295481+BitNexuz@users.noreply.github.com>
2021-04-09 23:05:49 -0300

    tarefa
    tarefa concluida

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+
+    Transform goal;
+
+    //setando atributos do tank
+
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+
+    //Importando as informações do wpmanager para o controle dos wpoints
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+    void Start()
+    {
+        //as informações do wpmanager são trazidas para as variaveis wps(controla os waypoinst para onde o tank deve ir) e g(contem as irformações do graphs)
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+       
+        currentNode = wps[0];
+    }
+
+    //controla para que o tank va para o heliporto
+    public void GoToHeli()
+    {
+        
+        g.AStar(currentNode, wps[9]);
+        currentWP = 0;
+    }
+
+    //controla para que o tank va para as ruinas
+    public void GoToRuin()
+    {
+      
+        g.AStar(currentNode, wps[3]);       
+        currentWP = 0;
+    }
+    //controla para que o tank va para a fabrica
+    public void GoToFabrica()
+    {
+ 
+        g.AStar(currentNode, wps[10]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        //caso não exista um wp configurado para que o tank va até ele, o codigo retorna
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+          return;
+
+        //O nó que estará mais próximo neste momento
+        currentNode = g.getPathPoint(currentWP);
+
+        //se estivermos mais próximo bastante do nó o tanque se moverá para o próximo
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position,transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        //caso o wp atual seja menor que para onde ele deve ir, o tank se move em direção ao proximo wp
+        if (currentWP < g.getPathLength())
+        {
+      
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position; this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
+        }
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/WPManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct Link
+{
+    //configurações das estruturas
+    public enum direction { UNI, BI }
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    //setando as variaveis que serão usadas (array de links, de waypoints e criando uma nova importação de graphs)
+    public GameObject[] waypoints;
+
+
+    public Link[] links;
+
+
+    public Graph graph = new Graph();
+
+
+    void Start()
+    {
+        //se o tamanho do array waypoints for maior que 0, para cada waypoint é adicionado um node
+        if (waypoints.Length > 0)
+        {
+         
+            foreach (GameObject wp in waypoints)
+            {
+              
+                graph.AddNode(wp);
+            }
+
+            //para cada link em links é adicionado a graphs um edge de acordo se é uni ou bi
+            foreach (Link l in links)
+            {
+               
+                graph.AddEdge(l.node1, l.node2);
+            
+                if (l.dir == Link.direction.BI)
+                  
+                    graph.AddEdge(l.node2, l.node1);
+            }
+        }
+    }
+    //traça o caminho entre os pontos
+    void Update()
+    {
+        graph.debugDraw();
+    }
+}
+
